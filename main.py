94e0adb9798eabb2890d56533e7774efa9a79279
Gary <garygaogsc@outlook.com>
2023-11-07 20:50:48 +0800

    add caltech dataset

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ import numpy as np
 
 
 
-# init()
 if __name__ == '__main__':
     load = LoadDatasets()
-    dataset = sys.argv[1]
-    anchor_number = int(sys.argv[2])
-    if type(anchor_number)!=int or anchor_number<=0:
-        print("Anchor Number need integer")
-        exit(1)
-    print("start load datasets")
-    X_train, y_train, X_test, y_test = None, None, None, None
-    match dataset:
-        case 'mnist':
-            X_train, y_train, X_test, y_test = load.load_mnist()
-        case 'usps':
-            X_train, y_train, X_test, y_test = load.load_usps()
-        case 'letter':
-            X_train, y_train, X_test, y_test = load.load_letter()
-        case 'caltech':
-            X_train, y_train, X_test, y_test = load.load_caltech()
-        case _:
-            print("Can not found dataset")
-            exit(1)
-    train_test(X_train, y_train, X_test, y_test, anchor_number)
+    load.load_caltech()
+    # dataset = sys.argv[1]
+    # anchor_number = int(sys.argv[2])
+    # if type(anchor_number)!=int or anchor_number<=0:
+    #     print("Anchor Number need integer")
+    #     exit(1)
+    # print("start load datasets")
+    # X_train, y_train, X_test, y_test = None, None, None, None
+    # match dataset:
+    #     case 'mnist':
+    #         X_train, y_train, X_test, y_test = load.load_mnist()
+    #     case 'usps':
+    #         X_train, y_train, X_test, y_test = load.load_usps()
+    #     case 'letter':
+    #         X_train, y_train, X_test, y_test = load.load_letter()
+    #     case 'caltech':
+    #         X_train, y_train, X_test, y_test = load.load_caltech()
+    #     case _:
+    #         print("Can not found dataset")
+    #         exit(1)
+    # train_test(X_train, y_train, X_test, y_test, anchor_number)
 
     # load = LoadDatasets()
     # X_train, y_train, X_test, y_test = load.load_usps()
